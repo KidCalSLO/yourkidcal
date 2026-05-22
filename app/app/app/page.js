405422bdca06b60a008384fada31bfcd41e8a736
KidCalSLO <yourkidcal@gmail.com>
2026-05-22 13:20:45 -0700

    Create page.js

---
+++ app/app/app/page.js
@@ -0,0 +1,14 @@
+import { supabase } from '../lib/supabase'
+import HomeClient from './HomeClient'
+
+export const revalidate = 60
+
+export default async function Home() {
+  const { data: listings } = await supabase
+    .from('listings')
+    .select('*')
+    .eq('status', 'approved')
+    .order('deadline', { ascending: true })
+
+  return <HomeClient listings={listings || []} />
+}
